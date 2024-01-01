c14d282a87fda8a3d7ace834e8421bdebd46370b
kolibri753 <kolibri753@gmail.com>
2024-01-01 05:14:10 +0200

    feat: add tabs for Jingle Bells

--- src/data/tabsData.ts
+++ src/data/tabsData.ts
@@ -1,8 +1,160 @@
 export const tabsEasy: { noteName: string; duration: number }[] = [
-  { noteName: "C4", duration: 5000 },
-  { noteName: "E4", duration: 5000 },
   { noteName: "G4", duration: 5000 },
-  { noteName: "C5", duration: 5000 },
   { noteName: "E5", duration: 5000 },
-  // Add more notes as needed
+  { noteName: "D5", duration: 5000 },
+  { noteName: "C5", duration: 5000 },
+  { noteName: "G4", duration: 5000 },
+
+  { noteName: "P", duration: 2000 },
+
+  { noteName: "G4", duration: 5000 },
+  { noteName: "E5", duration: 5000 },
+  { noteName: "D5", duration: 5000 },
+  { noteName: "C5", duration: 5000 },
+  { noteName: "A4", duration: 5000 },
+
+  { noteName: "P", duration: 2000 },
+
+  { noteName: "A4", duration: 5000 },
+  { noteName: "F5", duration: 5000 },
+  { noteName: "E5", duration: 5000 },
+  { noteName: "D5", duration: 5000 },
+  { noteName: "G5", duration: 5000 },
+
+  { noteName: "P", duration: 2000 },
+
+  { noteName: "G5", duration: 5000 },
+  { noteName: "G5", duration: 5000 },
+  { noteName: "A5", duration: 5000 },
+  { noteName: "G5", duration: 5000 },
+  { noteName: "F5", duration: 5000 },
+  { noteName: "D5", duration: 5000 },
+  { noteName: "E5", duration: 5000 },
+
+  { noteName: "P", duration: 2000 },
+
+  { noteName: "G4", duration: 5000 },
+  { noteName: "E5", duration: 5000 },
+  { noteName: "D5", duration: 5000 },
+  { noteName: "C5", duration: 5000 },
+  { noteName: "G4", duration: 5000 },
+
+  { noteName: "P", duration: 2000 },
+
+  { noteName: "G4", duration: 5000 },
+  { noteName: "E5", duration: 5000 },
+  { noteName: "D5", duration: 5000 },
+  { noteName: "C5", duration: 5000 },
+  { noteName: "A4", duration: 5000 },
+
+  { noteName: "P", duration: 2000 },
+
+  { noteName: "A4", duration: 5000 },
+  { noteName: "F5", duration: 5000 },
+  { noteName: "E5", duration: 5000 },
+  { noteName: "D5", duration: 5000 },
+  { noteName: "G5", duration: 5000 },
+
+  { noteName: "P", duration: 2000 },
+
+  { noteName: "G5", duration: 5000 },
+  { noteName: "G5", duration: 5000 },
+  { noteName: "A5", duration: 5000 },
+  { noteName: "G5", duration: 5000 },
+  { noteName: "F5", duration: 5000 },
+  { noteName: "D5", duration: 5000 },
+  { noteName: "C5", duration: 5000 },
+
+  { noteName: "P", duration: 2000 },
+
+  { noteName: "E5", duration: 5000 },
+  { noteName: "E5", duration: 5000 },
+  { noteName: "E5", duration: 5000 },
+
+  { noteName: "P", duration: 2000 },
+
+  { noteName: "E5", duration: 5000 },
+  { noteName: "E5", duration: 5000 },
+  { noteName: "E5", duration: 5000 },
+
+  { noteName: "P", duration: 2000 },
+
+  { noteName: "E5", duration: 5000 },
+  { noteName: "G5", duration: 5000 },
+  { noteName: "C5", duration: 5000 },
+  { noteName: "D5", duration: 5000 },
+  { noteName: "E5", duration: 5000 },
+
+  { noteName: "P", duration: 2000 },
+
+  { noteName: "F5", duration: 5000 },
+  { noteName: "F5", duration: 5000 },
+  { noteName: "F5", duration: 5000 },
+  { noteName: "F5", duration: 5000 },
+
+  { noteName: "P", duration: 2000 },
+
+  { noteName: "F5", duration: 5000 },
+  { noteName: "E5", duration: 5000 },
+  { noteName: "E5", duration: 5000 },
+
+  { noteName: "P", duration: 2000 },
+
+  { noteName: "E5", duration: 5000 },
+  { noteName: "E5", duration: 5000 },
+
+  { noteName: "P", duration: 2000 },
+
+  { noteName: "E5", duration: 5000 },
+  { noteName: "D5", duration: 5000 },
+  { noteName: "D5", duration: 5000 },
+  { noteName: "E5", duration: 5000 },
+  { noteName: "D5", duration: 5000 },
+  { noteName: "G5", duration: 5000 },
+
+  { noteName: "P", duration: 2000 },
+
+  { noteName: "E5", duration: 5000 },
+  { noteName: "E5", duration: 5000 },
+  { noteName: "E5", duration: 5000 },
+
+  { noteName: "P", duration: 2000 },
+
+  { noteName: "E5", duration: 5000 },
+  { noteName: "E5", duration: 5000 },
+  { noteName: "E5", duration: 5000 },
+
+  { noteName: "P", duration: 2000 },
+
+  { noteName: "E5", duration: 5000 },
+  { noteName: "G5", duration: 5000 },
+  { noteName: "C5", duration: 5000 },
+  { noteName: "D5", duration: 5000 },
+  { noteName: "E5", duration: 5000 },
+
+  { noteName: "P", duration: 2000 },
+
+  { noteName: "F5", duration: 5000 },
+  { noteName: "F5", duration: 5000 },
+  { noteName: "F5", duration: 5000 },
+  { noteName: "F5", duration: 5000 },
+
+  { noteName: "P", duration: 2000 },
+
+  { noteName: "F5", duration: 5000 },
+  { noteName: "E5", duration: 5000 },
+  { noteName: "E5", duration: 5000 },
+
+  { noteName: "P", duration: 2000 },
+
+  { noteName: "E5", duration: 5000 },
+  { noteName: "E5", duration: 5000 },
+
+  { noteName: "P", duration: 2000 },
+
+  { noteName: "G5", duration: 5000 },
+  { noteName: "G5", duration: 5000 },
+  { noteName: "F5", duration: 5000 },
+  { noteName: "D5", duration: 5000 },
+  { noteName: "C5", duration: 5000 },
 ];
