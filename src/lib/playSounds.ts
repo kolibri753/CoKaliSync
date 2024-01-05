42b1f2c76e48166a42eba7736ac4dae501967d78
kolibri753 <kolibri753@gmail.com>
2024-01-05 05:11:06 +0200

    refactor: simplify playSounds function and move kalimbaKeySounds to the public folder
    - Removed unnecessary dependencies in 'playSounds'.
    - Simplified by directly using 'notesDistribution' from the 'types/KalimbaNote'.
    - Updated the path for loading audio in the 'playSounds' function
      (public folder).
    - Moved the 'kalimbaKeySounds' to the public folder to prevent deletion during the build process.

--- src/lib/playSounds.ts
+++ src/lib/playSounds.ts
@@ -1,17 +1,20 @@
 import { OnChangeAction } from "rune-games-sdk";
-import { GameActions, GameState } from "../logic";
+import { GameActions } from "../logic";
+import { notesDistribution } from "../types/KalimbaNote";
 
 export default function playSounds(
-  game: GameState,
-  previousGame: GameState,
   action: OnChangeAction<GameActions> | undefined
 ) {
   if (action?.name === "playNote") {
-    const note = game.kalimbaNotes.find(
-      (note) => note.name === action.params.noteName
-    );
-    if (!note) return console.error("Note could not be determined");
-    const audio = new Audio(import.meta.env.BASE_URL + `src/assets/kalimbaKeySounds/${note.name}.m4a`);
+    const noteName = action.params.noteName;
+
+    if (!notesDistribution.includes(noteName)) {
+      console.error("Note could not be determined");
+      return;
+    }
+
+    const audio = new Audio(`kalimbaKeySounds/${noteName}.m4a`);
+
     audio.play();
   }
 }
